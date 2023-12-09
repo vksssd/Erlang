62c3d23ec9c57973b995c3097647169c228fe5d3
vinayak kumar singh <36007729+vksssd@users.noreply.github.com>
2023-12-09 02:28:13 +0000

    error

--- basics/message_passing.erl
+++ basics/message_passing.erl
@@ -4,18 +4,55 @@
 start_server()->
     spawn(fun server/0).
 
-server()->
+% server()->
+%     receive
+%         {From,Message}->
+%             From ! {self(), "Server received: "++ Message},
+%             server();
+%         stop ->
+%             io:format("Server stopping.~n")
+% end.
+
+% client(ServerPid)->
+%     ServerPid ! {self(), "Hello from the client."},
+%     receive
+%         {ServerPid, Reply} -> io:format("Client received: ~p~n",[Reply])
+% end,
+% ServerPid ! stop.
+
+server() ->
     receive
-        {From,Message}->
-            From ! {self(), "Server received: "++ Message},
+        {broadcast, Message} ->
+            io:format("Broadcasting: ~p~n",[Message]),
+            broadcast(Message),
+            server();
+        {From, Message}->
+            From ! {self(), "Server received: "++Message},
             server();
         stop ->
-            io:format("Server stopping.~n")
+            io:format("Stoping server.~n")
 end.
 
+% broadcast(Message)->
+%     lists:foreach(fun(Pid) -> Pid ! {broadcast, Message} end, registered()).
+
+broadcast(Message) ->
+    Clients = [Pid || Pid <- registered(), is_client(Pid)],
+    lists:foreach(fun(Pid) -> Pid ! {broadcast, Message} end, Clients).
+
+is_client(Pid) ->
+    case whereis(Pid) of
+        undefined -> false;
+        _ -> true
+    end.
+
+
 client(ServerPid)->
-    ServerPid ! {self(), "Hello from the client."},
+    ServerPid ! {self(),"Hello from client."},
     receive
-        {ServerPid, Reply} -> io:format("Client received: ~p~n",[Reply])
-end,
-ServerPid ! stop.
+        {ServerPid, Reply} -> io:format("Client received: ~p~n", [Reply]);
+        {broadcast, BroadcastMsg} -> io:format("Broadcast message: ~p~n", [BroadcastMsg])
+    end,
+
+ServerPid ! {broadcast, "This is a broadcast message."}.
+
